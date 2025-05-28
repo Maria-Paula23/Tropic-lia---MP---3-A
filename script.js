44c3d5851e167f77f6577857ec50dea46afaf906
Maria-Paula23 <132026592+Maria-Paula23@users.noreply.github.com>
2025-05-28 11:00:12 +0000

    Terminamos a aula 6.

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+    const alternaContraste = document.getElementById('alterna-contraste');
 
     let tamanhoAtualFonte = 1;
-    aumentaFonteBotao.addEventListener('click', function(){
+    aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
 
-    diminuiFonteBotao.addEventListener('click', function(){
+    diminuiFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
 
-    botaoDeAcessibilidade.addEventListener('click', function (){
+    botaoDeAcessibilidade.addEventListener('click', function () {
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
-       });
+    });
+
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    });
 
 });
